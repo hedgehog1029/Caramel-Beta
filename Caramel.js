21fd5a989608e906099dfb94c8f8569901d6d64a
kurisubrooks <Kurisu@Kurisus-iMac.local>
2014-10-26 18:04:12 +1100

    Init

---
+++ Caramel.js
@@ -0,0 +1,43 @@
+/*
+ *  Caramel © 2014 : ALL RIGHTS RESERVED
+ *
+ *  PUBLISHED UNDER [ CC-NC 3.0 AU ]
+ *  https://creativecommons.org/licenses/by-nc/3.0/au/
+ *
+ *  HTML/CSS    - @kurisubrooks
+ *  JAVASCRIPT  - @Penagwin
+ *  FONTAWESOME - @DaveGandy - http://fontawesome.io/
+ */
+
+$(document).ready(function() {
+
+// Navbar Collapse
+
+    $(".nav .bar").css({"display": "block"});   
+     //On resize style the nav bars
+        $(window).resize(function() {
+            if ($(window).width() >= 600) {
+                $(".bar li").css({
+                    "display": "inline-block"
+                });
+                $(".bar .collapse").css({
+                    "display": "none"
+                });
+
+            } else {
+
+                $(".bar .collapse").css({"display": "block"});
+                $(".bar li").css({ "display": "none"});
+                $(".bar").css({"max-height": "inherit"});
+            }
+        }); 
+
+        //On Expand Pressed
+        $(".bar .collapse").click(function() {
+            $(this).siblings('a').children('li').slideToggle('medium');
+
+        });
+
+        //Force a resize to init.
+        $(window).resize();
+    });
---
+++ README.md
@@ -0,0 +1,27 @@
+![image](https://raw.githubusercontent.com/kurisubrooks/Caramel/gh-pages/Logo.png)
+
+A CSS Framework that's as pure as Caramel.
+
+Want a live demo? [Click Here!](http://kurisubrooks.github.io/Caramel/)
+
+
+## License
+
+Creative Commons - Attribution, Non Commercial - 3.0 Australia
+(CC BY-NC 3.0 AU)
+
+
+### YOU CAN:
+
+* Use this in Non-Commercial Projects
+* Redistribute modified works
+
+### YOU CAN'T:
+
+* Use this Commercially (Make Money off it)
+* Sublicense
+* Give a Warranty/Guarantee
+
+### YOU MUST:
+
+* Include Copyright in Modified Copies
